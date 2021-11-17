3fde6c9808a9700042018bebedf3ebf3bf39e0aa
Iván Santa <santatasan@hotmail.com>
2021-11-17 20:12:53 +0100

    Files created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    FormularioComponent,
   ],
   imports: [
     BrowserModule,
